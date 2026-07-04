432deee3f15fadcacecd90dfa4765c59293a9748
Pham Nguyen Tuong Vy <vyb2206025@student.ctu.edu.vn>
2026-07-04 19:27:54 +0700

    Create a contact listing page

---
+++ src/services/contacts.service.js
@@ -0,0 +1,87 @@
+import { DEFAULT_AVATAR } from '@/constants';
+
+/**
+ * @param {string} url 
+ * @param {RequestInit} options 
+ * @returns Promise<Response>
+ */
+async function efetch(url, options = {}) {
+  let response = {};
+  let json = {};
+
+  try {
+    response = await fetch(url, options);
+    json = (await response.json()) ?? {};
+  } catch (error) {
+    throw new Error(error.message);
+  }
+
+  if (!response.ok || json.status !== 'success') {
+    throw new Error(json.message ?? response?.statusText);
+  }
+
+  return json.data;
+}
+
+function makeContactsService() {
+  const baseUrl = '/api/v1/contacts';
+
+  async function fetchContacts(page, limit = 10) {
+    let url = `${baseUrl}?page=${page}&limit=${limit}`;
+    const data = await efetch(url);
+
+    data.contacts = data.contacts.map((contact) => {
+      return {
+        ...contact,
+        avatar: contact.avatar ?? DEFAULT_AVATAR,
+      };
+    });
+
+    return data;
+  }
+
+  async function fetchContact(id) {
+    const { contact } = await efetch(`${baseUrl}/${id}`);
+    return {
+      ...contact,
+      avatar: contact.avatar ?? DEFAULT_AVATAR
+    };
+  }
+
+  async function createContact(contact) {
+    return efetch(baseUrl, {
+      method: 'POST',
+      body: contact,
+    });
+  }
+
+  async function deleteAllContacts() {
+    return efetch(baseUrl, {
+      method: 'DELETE',
+    });
+  }
+
+  async function updateContact(id, contact) {
+    return efetch(`${baseUrl}/${id}`, {
+      method: 'PUT',
+      body: contact,
+    });
+  }
+
+  async function deleteContact(id) {
+    return efetch(`${baseUrl}/${id}`, {
+      method: 'DELETE',
+    });
+  }
+
+  return {
+    fetchContacts,
+    fetchContact,
+    createContact,
+    updateContact,
+    deleteContact,
+    deleteAllContacts,
+  };
+}
+
+export default makeContactsService();
---
+++ src/views/ContactBook.vue
@@ -0,0 +1,130 @@
+<script setup>
+import { ref, computed, watch } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+import ContactCard from '@/components/ContactCard.vue';
+import InputSearch from '@/components/InputSearch.vue';
+import ContactList from '@/components/ContactList.vue';
+import MainPagination from '@/components/MainPagination.vue';
+import contactsService from '@/services/contacts.service';
+
+const router = useRouter();
+const route = useRoute();
+
+const totalPages = ref(1);
+
+const currentPage = computed(() => {
+  const page = Number(route.query?.page);
+  if (Number.isNaN(page) || page < 1) return 1;
+  return page;
+});
+
+const contacts = ref([]);
+const selectedIndex = ref(-1);
+const searchText = ref('');
+
+const searchableContacts = computed(() =>
+  contacts.value.map((contact) => {
+    const { name, email, address, phone } = contact;
+    return [name, email, address, phone].join('');
+  })
+);
+
+const filteredContacts = computed(() => {
+  if (!searchText.value) return contacts.value;
+  return contacts.value.filter((contact, index) =>
+    searchableContacts.value[index].includes(searchText.value)
+  );
+});
+
+const selectedContact = computed(() => {
+  if (selectedIndex.value < 0) return null;
+  return filteredContacts.value[selectedIndex.value];
+});
+
+async function retrieveContacts(page) {
+  try {
+    const chunk = await contactsService.fetchContacts(page);
+    totalPages.value = chunk.metadata.lastPage ?? 1;
+    contacts.value = chunk.contacts.sort(
+      (current, next) => current.name.localeCompare(next.name)
+    );
+    selectedIndex.value = -1;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function onDeleteContacts() {
+  if (confirm('Bạn muốn xóa tất cả Liên hệ?')) {
+    try {
+      await contactsService.deleteAllContacts();
+      totalPages.value = 1;
+      contacts.value = [];
+      selectedIndex.value = -1;
+      changeCurrentPage(1);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+function goToAddContact() {
+  router.push({ name: 'contact.add' });
+}
+
+function changeCurrentPage(page) {
+  router.push({ name: 'contactbook', query: { page } });
+}
+
+watch(searchText, () => (selectedIndex.value = -1));
+watch(currentPage, () => retrieveContacts(currentPage.value), { immediate: true });
+</script>
+
+<template>
+  <div class="page row mb-5">
+    <div class="mt-3 col-md-6">
+      <h4>Danh bạ <i class="fas fa-address-book"></i></h4>
+      <div class="my-3">
+        <InputSearch v-model="searchText" />
+      </div>
+      <ContactList
+        v-if="filteredContacts.length > 0"
+        :contacts="filteredContacts"
+        v-model:selected-index="selectedIndex"
+      />
+      <p v-else>Không có liên hệ nào.</p>
+
+      <div class="mt-3 d-flex flex-wrap justify-content-round align-items-center">
+        <MainPagination
+          :total-pages="totalPages"
+          :current-page="currentPage"
+          @update:current-page="changeCurrentPage"
+        />
+        <div class="w-100"></div>
+        <button class="btn btn-sm btn-primary" @click="retrieveContacts(currentPage)">
+          <i class="fas fa-redo"></i> Làm mới
+        </button>
+        <button class="btn btn-sm btn-success" @click="goToAddContact">
+          <i class="fas fa-plus"></i> Thêm mới
+        </button>
+        <button class="btn btn-sm btn-danger" @click="onDeleteContacts">
+          <i class="fas fa-trash"></i> Xóa tất cả
+        </button>
+      </div>
+    </div>
+
+    <div class="mt-3 col-md-6">
+      <div v-if="selectedContact">
+        <h4>Chi tiết Liên hệ <i class="fas fa-address-card"></i></h4>
+        <ContactCard :contact="selectedContact" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.page {
+  text-align: left;
+  max-width: 750px;
+}
+</style>
